34389a81a60d665e27093aabb48a28a7ab1b0c24
ShukiSasakura <sasakura2023@s.okayama-u.ac.jp>
2023-10-06 18:11:20 +0900

    Change sending message to 0~255 number

--- receiver/src/main.rs
+++ receiver/src/main.rs
@@ -6,25 +6,25 @@ fn main() {
     let host_and_port = "localhost:5555";
     let mut addrs = host_and_port.to_socket_addrs().unwrap();
 
-    if let Some(addr) = addrs.find(|x| (*x).is_ipv4()) {
+    let Some(addr) = addrs.find(|x| (*x).is_ipv4()) else {
+        eprintln!("Invalid Host:Port Number");
+        return;
+    };
 
-        match TcpStream::connect(addr) {
-            Err(_) => {
-                println!("Failed connect");
-            }
-            Ok(mut stream) => {
-                println!("connect");
-                for i in 0..10000 {
-                    let mut buf: Vec<u8> = Vec::new();
-                    buf.resize(1024, 0);
-                    stream.read_exact(&mut buf[..]);
-                    buf.truncate("Send Message".as_bytes().len());
-                    let msg = String::from_utf8(buf).unwrap();
-                    println!("Receive message {}: {}", i, msg);
-                }
+    match TcpStream::connect(addr) {
+        Err(_) => {
+            println!("Failed connect");
+        }
+        Ok(mut stream) => {
+            println!("connect");
+            for i in 0..256 {
+                let mut buf: Vec<u8> = vec![0];
+                // buf.resize(1024, 0);
+                let _ = stream.read_exact(&mut buf);
+                // buf.truncate("Send Message".as_bytes().len());
+                // let msg = String::from_utf8(buf).unwrap();
+                println!("Receive message {}: {:?}", i, buf);
             }
         }
-     } else {
-         eprintln!("Incalid Host:Port Number");
-     };
+    }
 }
--- write_server_wasmedge/src/main.rs
+++ write_server_wasmedge/src/main.rs
@@ -1,29 +1,27 @@
 use std::io;
-use std::io::{Read, Write, ErrorKind};
+use std::io::Write;
 use std::thread;
 use std::time;
 use wasmedge_wasi_socket::{ToSocketAddrs, TcpListener, TcpStream};
 
 fn main() -> io::Result<()> {
+    let mut i = 0;
     let listener = TcpListener::bind("127.0.0.1:5555", false).unwrap();
     let mut stream  = match listener.accept(true) {
         Ok((stream, _)) => stream ,
         Err(e) => return Err(e)
     };
     loop {
-        send_msg(&mut stream);
+        let msg: [u8; 1] = [i];
+        let _ = send_msg(&mut stream, &msg);
         // thread::sleep(std::time::Duration::from_micros(10));
+        i += 1;
     }
 }
 
-fn send_msg(stream: &mut TcpStream) -> Result<(), std::io::Error> {
-    let mut msg = "Send Message";
-    let mut buf = [0u8; 1024];
-    unsafe {
-        let msg = msg.as_bytes();
-        std::ptr::copy_nonoverlapping(msg.as_ptr(), buf.as_mut_ptr(), msg.len());
-    }
-    stream.write(& buf[..]);
-    // stream.write_all(& buf[..]);
+fn send_msg(stream: &mut TcpStream, msg: &[u8]) -> Result<(), std::io::Error> {
+    // let msg = msg.as_bytes();
+    // let _ = stream.write(& msg);
+    let _ = stream.write_all(msg);
     Ok(())
 }
