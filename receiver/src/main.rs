2baaef555cf7e8e9609cdf86255dab5e1d0426bc
ShukiSasakura <sasakura2023@s.okayama-u.ac.jp>
2023-10-12 15:21:13 +0900

    Change sending message to 0-255 number

--- receiver/src/main.rs
+++ receiver/src/main.rs
@@ -1,6 +1,6 @@
 use std::net::{ToSocketAddrs, TcpListener, TcpStream};
 use std::io::Read;
-use std::str;
+// use std::str;
 
 fn main() {
     let host_and_port = "localhost:5555";
@@ -16,14 +16,22 @@ fn main() {
             println!("Failed connect");
         }
         Ok(mut stream) => {
-            println!("connect");
-            for i in 0..256 {
-                let mut buf: Vec<u8> = vec![0];
-                // buf.resize(1024, 0);
+            let mut i: u32 = 0;
+            // println!("connect");
+            loop {
+                // let mut buf: Vec<u8> = vec![0];
+                let mut buf: [u8; 1] = [0];
                 let _ = stream.read_exact(&mut buf);
-                // buf.truncate("Send Message".as_bytes().len());
-                // let msg = String::from_utf8(buf).unwrap();
-                println!("Receive message {}: {:?}", i, buf);
+                // if (i % 1000000) == 0 {
+                    println!("Receive message {}: {:?}", i, buf[0]);
+                    // println!("{}",buf[0]);
+                // }
+                if (i % 256) != buf[0].into() {
+                    // println!("Receive message {}: {:?}", i, buf[0]);
+                    // println!("Error-----------------------------------");
+                    break;
+                }
+                i += 1;
             }
         }
     }
--- write_server_wasmedge/src/main.rs
+++ write_server_wasmedge/src/main.rs
@@ -1,5 +1,6 @@
 use std::io;
 use std::io::Write;
+use std::println;
 use std::thread;
 use std::time;
 use wasmedge_wasi_socket::{ToSocketAddrs, TcpListener, TcpStream};
@@ -13,15 +14,44 @@ fn main() -> io::Result<()> {
     };
     loop {
         let msg: [u8; 1] = [i];
-        let _ = send_msg(&mut stream, &msg);
+        send_msg(&mut stream, &msg)?;
         // thread::sleep(std::time::Duration::from_micros(10));
-        i += 1;
+        if i == 255 {
+            i = 0;
+        } else {
+            i += 1;
+        }
     }
 }
 
 fn send_msg(stream: &mut TcpStream, msg: &[u8]) -> Result<(), std::io::Error> {
     // let msg = msg.as_bytes();
-    // let _ = stream.write(& msg);
-    let _ = stream.write_all(msg);
+    // let _ = stream.write(msg);
+    // stream.write_all(msg)?;
+    write_all(stream, msg)?;
     Ok(())
 }
+
+fn write_all(stream: &mut TcpStream, mut buf: &[u8]) -> Result<(), std::io::Error> {
+        while !buf.is_empty() {
+            match stream.write(buf) {
+                Ok(0) => {
+                    println!("send 0 size message");
+                    break;
+                }
+                Ok(n) => buf = &buf[n..],
+                Err(e) => {
+                    //"Resource temporarily unavailable (os error6)のときにsleep"
+                    if e.kind() != io::ErrorKind::WouldBlock {
+                        return Err(e);
+                    } else {
+                        println!("WouldBlock");
+                        // println!("sleep");
+                        // thread::sleep(std::time::Duration::from_micros(10));
+                    }
+                }
+
+            }
+        }
+        Ok(())
+    }
