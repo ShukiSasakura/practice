e7517d09e427134cc864f33849c2a95ade85720b
ShukiSasakura <sasakura2023@s.okayama-u.ac.jp>
2023-11-06 12:40:55 +0900

    Sent when read times out. Calculate and display the probability that write returns WouldBlock

--- write_server_wasmedge/src/main.rs
+++ write_server_wasmedge/src/main.rs
@@ -1,57 +1,78 @@
 use std::io;
+use std::io::Read;
 use std::io::Write;
-use std::println;
 use std::thread;
 use std::time;
 use wasmedge_wasi_socket::{ToSocketAddrs, TcpListener, TcpStream};
 
 fn main() -> io::Result<()> {
-    let mut i = 0;
     let listener = TcpListener::bind("127.0.0.1:5555", false).unwrap();
     let mut stream  = match listener.accept(true) {
         Ok((stream, _)) => stream ,
         Err(e) => return Err(e)
     };
-    loop {
-        let msg: [u8; 1] = [i];
-        send_msg(&mut stream, &msg)?;
-        // thread::sleep(std::time::Duration::from_micros(10));
-        if i == 255 {
-            i = 0;
-        } else {
-            i += 1;
+
+    let mut counter = 0;
+
+    for i in (0..=255).cycle().take(256 * 400) {
+        let msg: [u8; 1024] = [i; 1024];
+        let mut recv_buf: [u8; 1024] = [0; 1024];
+        match stream.read_exact(&mut recv_buf) {
+            Ok(_) => {},
+            Err(e) => {
+                if e.kind() == io::ErrorKind::WouldBlock {
+                 send_msg(&mut stream, &msg, &mut counter)?;
+                }
+            }
         }
     }
-}
-
-fn send_msg(stream: &mut TcpStream, msg: &[u8]) -> Result<(), std::io::Error> {
-    // let msg = msg.as_bytes();
-    // let _ = stream.write(msg);
-    // stream.write_all(msg)?;
-    write_all(stream, msg)?;
+    let probability: f32 = (counter as f32/(256.0 * 400.0))*100.0;
+    println!("WouldBlock num: {}, probability: {}", counter, probability);
     Ok(())
 }
 
-fn write_all(stream: &mut TcpStream, mut buf: &[u8]) -> Result<(), std::io::Error> {
-        while !buf.is_empty() {
-            match stream.write(buf) {
-                Ok(0) => {
-                    println!("send 0 size message");
-                    break;
-                }
-                Ok(n) => buf = &buf[n..],
-                Err(e) => {
-                    //"Resource temporarily unavailable (os error6)のときにsleep"
-                    if e.kind() != io::ErrorKind::WouldBlock {
-                        return Err(e);
-                    } else {
-                        println!("WouldBlock");
-                        // println!("sleep");
-                        // thread::sleep(std::time::Duration::from_micros(10));
-                    }
-                }
+fn send_msg(stream: &mut TcpStream, msg: &[u8], counter: &mut u32) -> Result<(), std::io::Error> {
+    // let msg = msg.as_bytes();
+    // stream.write_all(msg);
+    write_all(stream, msg, counter)?;
+    Ok(())
+}
 
+fn write_all(stream: &mut TcpStream, mut buf: &[u8], counter: &mut u32) -> Result<(), std::io::Error> {
+    let mut i = 0;
+    let mut fib1 = 0;
+    let mut fib2 = 1;
+    while !buf.is_empty() {
+        match stream.write(buf) {
+            Ok(0) => {
+                println!("send 0 size message");
+                break;
+            }
+            Ok(1024) => {
+                buf = &buf[1024..];
+            }
+            Ok(n) => {
+                buf = &buf[n..];
+            }
+            Err(e) => {
+                //"Resource temporarily unavailable (os error6)のときに再送"
+                if e.kind() == io::ErrorKind::WouldBlock {
+                    i += 1;
+                    if i == 1 {
+                        *counter += 1;
+                    }
+                    if i > 1 {
+                        thread::sleep(std::time::Duration::from_micros(fib1+fib2));
+                        let mut temp = fib1+fib2;
+                        println!("{}", i);
+                        fib1 = fib2;
+                        fib2 = temp;
+                    }
+                } else {
+                    return Err(e);
+                }
             }
         }
-        Ok(())
     }
+    Ok(())
+}
