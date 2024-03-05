9e9c8b1fe990f37f30147d1ebbbf53013d040498
ShukiSasakura <pbje8wfi@s.okayama-u.ac.jp>
2024-03-05 15:48:36 +0900

    Update sleep sec

--- wasi-threads-simple/src/main.rs
+++ wasi-threads-simple/src/main.rs
@@ -5,12 +5,12 @@ fn main() {
    thread::spawn(|| {
        for i in 1..5 {
            println!("Hi number {} from the spawned thread!", i);
-           thread::sleep(Duration::from_millis(1));
+           thread::sleep(Duration::from_secs(3));
        }
    });
 
    for j in 1..10 {
        println!("Hi number {} from the main thread!", j);
-       thread::sleep(Duration::from_millis(1));
+       thread::sleep(Duration::from_secs(2));
    }
 }
